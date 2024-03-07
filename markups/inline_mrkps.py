cbc29d5a4d71a3a006256f876e7b8b08923e0b71
goga123456 <110159417+goga123456@users.noreply.github.com>
2024-03-07 13:44:32 +0500

    Delete markups directory

--- markups/inline_mrkps.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-
-def get_agree_kb() -> InlineKeyboardMarkup:
-    kb1 = InlineKeyboardMarkup(resize_keyboard=True)
-    b1 = InlineKeyboardButton('Продолжить', callback_data='next')
-    b2 = InlineKeyboardButton('Отказаться', callback_data='close')
-    kb1.add(b1, b2)
-    return kb1
